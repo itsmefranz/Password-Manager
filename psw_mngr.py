58b5331e09eee8364140874fcdfd38e425730f64
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-19 12:11:45 +0800

    settled conditions on the beginning of the program

--- psw_mngr.py
+++ psw_mngr.py
@@ -1,1 +1,11 @@
 pwd= input("What is the master password? ")
+
+while True:
+    mode= input("Would you like to add a new password or view existing ones (view, add)? you can press q to quit. ")
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode.")
+        continue
