49f0c50b01ba6a31d0cdf57b95e5498261642c13
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-19 12:39:07 +0800

    defined add function for now along with the additional settled conditions

--- psw_mngr.py
+++ psw_mngr.py
@@ -1,11 +1,24 @@
-pwd= input("What is the master password? ")
+master_pwd= input("What is the master password? ")
+
+def view():
+    pass
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + pwd + '\n')
 
 while True:
-    mode= input("Would you like to add a new password or view existing ones (view, add)? you can press q to quit. ")
+    mode= input("Would you like to add a new password or view existing ones (view, add)? you can press q to quit. ").lower()
+    if mode == "q":
+        break
+
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode.")
         continue
