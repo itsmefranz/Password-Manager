479bc1341edf33d2ccfa8f1f2f11172ddd826369
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-19 13:33:38 +0800

    added more defined function for the decrypting stuff

--- psw_mngr.py
+++ psw_mngr.py
@@ -2,12 +2,17 @@ from cryptography.fernet import Fernet
 
 master_pwd= input("What is the master password? ")
 
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
-
-write_key()
+'''
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
 
 def view():
     with open('password.txt', 'r') as f:
