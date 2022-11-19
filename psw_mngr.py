12d09362efbcce6a75543a4ec03efa1a638fa54e
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-19 12:45:20 +0800

    defined view function

--- psw_mngr.py
+++ psw_mngr.py
@@ -1,7 +1,9 @@
 master_pwd= input("What is the master password? ")
 
 def view():
-    pass
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
 
 def add():
     name = input("Account Name: ")
